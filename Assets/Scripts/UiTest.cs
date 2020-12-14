399596dfd45afea371e7c0490ecca90e993c4879
helei <969147832@qq.com>
2020-12-14 15:06:18 +0800

    初版提交

---
+++ Assets/Scripts/UiTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiTest : MonoBehaviour , IMassageInterface
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool HandleMessage(int id, object param1, object param2)
+    {
+        return false;
+    }
+
+
+    public int MessagePriority() {
+
+        return 1;
+    }
+}
